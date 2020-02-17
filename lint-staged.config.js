655ab53b0e18d39f393d1558894bd2494efbe4b7
Robert Guss <robert.guss@insuremytrip.com>
2020-02-17 16:07:57 -0500

    updated paths in lint staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  'resources/**/*.{js}': ['npm run lint:eslint', 'npm run lint:prettier'],
-  'resources/**/*.{scss,css}': [
+  'resources/js/**/*.{js}': ['npm run lint:eslint', 'npm run lint:prettier'],
+  'resources/scss/**/*.{scss,css}': [
     'npm run lint:stylelint',
     'npm run lint:prettier',
   ],
