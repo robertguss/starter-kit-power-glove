f983af9a1193da89dc45d729b4c9bc02e1720bc4
Robert Guss <robert.guss@insuremytrip.com>
2020-02-17 16:05:29 -0500

    setup purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,17 @@
+const autoprefixer = require('autoprefixer');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: ['./resources/**/*.html', './resources/**/*.vue'],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+const tailwindcss = require('tailwindcss');
+
 module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
 };
